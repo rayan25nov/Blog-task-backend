7e5bd9a3ca4cce6aa7c5a8364617494e1fe879e3
Rayan Ahmad <rayan25nov@gmail.com>
2025-03-07 15:08:28 +0530

    update

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,12 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // Initialize environment variables
 dotenv.config();
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Importing the Database Connection
 import dbConnection from "./db/config.js";
@@ -26,11 +22,12 @@ cloudinaryConnect();
 
 // Swagger setup
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// Serve static files from node_modules/swagger-ui-dist
-app.use(
-  "/api-docs",
-  express.static(path.join(__dirname, "node_modules", "swagger-ui-dist"))
-);
+
+// Serve Swagger JSON
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/swagger.json", (_, res) => {
+  res.json(swaggerSpec);
+});
 
 // Middlewares
 app.use(express.json());
